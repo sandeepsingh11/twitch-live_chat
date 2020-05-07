ad52938a599f60df4b5321b023f2c4e07432a3fe
Sandeep Singh <serperior2@gmail.com>
2020-05-07 19:41:16 -0400

    chat and live user functionality

--- app.js
+++ app.js
@@ -3,25 +3,78 @@ const app = express();
 const server = require('http').createServer(app);
 const io = require('socket.io')(server);
 
+// custom namespaces (chat servers)
+const catServer = io.of('/cat');
+const birdServer = io.of('/bird');
+const dogServer = io.of('/dog');
+
 
 
 // register public folder
 app.use(express.static(__dirname + '/public/') );
 
 // home route
-app.get('/', (req, res) => res.sendFile(__dirname + './public/index.html') );
+app.get('/', (req, res) => res.sendFile(__dirname + '/public/index.html') );
+app.get('/cat', (req, res) => res.sendFile(__dirname + '/public/cat.html') );
 
 
+
+// GLOBAL - live users
+var liveUsers = [];
 
 // socket.io connection
 io.on('connection', (socket) => {
     console.log('a user has connected');
 
+    // user has enter the chat
+    socket.on('new user', function(username) {
+        socket.username = username;
+
+        // update live users
+        liveUsers.push(username);
+
+
+        // update user display
+        // goes to: ./public/js/chat.js
+        io.emit('refresh users', liveUsers);
+    });
+
     // new chat message
     socket.on('chat message', function(message) {
+        
         // goes to: ./public/js/chat.js
-        io.emit('chat message', message);
-    })
+        io.emit('chat message', socket.username, message);
+    });
+
+    socket.on('disconnect', function() {
+        console.log('a user has disconnected');
+    });
+});
+
+
+// cat chat server
+catServer.on('connection', (socket) => {
+    console.log('a user has connected');
+
+    // user has enter the chat
+    socket.on('new user', function(username) {
+        socket.username = username;
+
+        // update live users
+        liveUsers.push(username);
+
+
+        // update user display
+        // goes to: ./public/js/chat.js
+        catServer.emit('refresh users', liveUsers);
+    });
+
+    // new chat message
+    socket.on('chat message', function(message) {
+        
+        // goes to: ./public/js/chat.js
+        catServer.emit('chat message', socket.username, message);
+    });
 
     socket.on('disconnect', function() {
         console.log('a user has disconnected');
--- public/js/chat.js
+++ public/js/chat.js
@@ -1,8 +1,38 @@
 $(document).ready(function() {
-    var socket = io();
+    var path = $(location).attr('pathname');
+    // specify chat server
+    var socket = io(path);
+
+    function getServer() {
+        // get path
+        var path = $(location).attr('pathname');
+        // set it
+        socket = io(path);
+
+        return socket;
+    }
+
+    // new user
+    $('#usernameForm').submit(function(e) {
+        // get username
+        var username = $('#username').val();
+
+        // hide username prompt
+        var usernameContainer = $('#usernameContainer');
+        $(usernameContainer).css('display', 'none');
+        $(usernameContainer).css('z-index', '-1');
+
+        // goes to: ../../app.js
+        socket.emit('new user', username );
+
+        // prevent page refresh
+        return false;
+    });
+
+
 
     // when user submits a message
-    $('form').submit(function(e) {
+    $('#messageForm').submit(function(e) {
         // get message
         var inputVal = $('#message').val();
 
@@ -16,9 +46,28 @@ $(document).ready(function() {
         return false;
     });
 
+
+
+
     // new chat message func
-    socket.on('chat message', function(message) {
+    socket.on('chat message', function(user, message) {
+        var chatMessage = `${user}: ${message}`;
+
         // append new chat message
-        $('#messages').append($('<p>').text(message));
+        $('#messages').append($('<p>').text(chatMessage));
+    });
+
+    // update live users display func
+    socket.on('refresh users', function(liveUsers) {
+        console.log(liveUsers);
+
+        // remove previous user list
+		$('#userList').children().remove();
+
+        // populate the user list
+        for (user of liveUsers) {
+            // update live user list
+            $('#userList').append($('<li>').text(user));
+        }
     });
 });
